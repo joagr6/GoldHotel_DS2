f2a35ef23f4bf545e5d87325e21485626cc48bf0
João Victor Gilioli da Rosa <joao.vgr@aluno.ifsc.edu.br>
2025-10-21 17:12:53 -0300

    Criaçao dos Migrates

---
+++ GolgHotel_DS2/database/migrations/2025_10_21_193938_create_hospedes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hospedes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',100);
+            $table->string('cpf',16);
+            $table->date('data_nascimento',10);
+            $table->string('telefone',20)->nullable();
+            $table->string('email',50)->nullable();
+            $table->string('cidade',50)->nullable();
+            $table->string('numcasa',50)->nullable();
+            $table->string('rua',50)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hospedes');
+    }
+};
